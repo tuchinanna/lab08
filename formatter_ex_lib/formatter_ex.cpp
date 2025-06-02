b351c9c32e800df70007ff66b458248b830c5408
tuchinanna <a.tuchina.anna@yandex.com>
2025-06-02 03:10:34 +0300

    Update formatter_ex.cpp

--- formatter_ex_lib/formatter_ex.cpp
+++ formatter_ex_lib/formatter_ex.cpp
@@ -1,8 +1,17 @@
-#include "formatter_ex.h"
-
 #include "formatter.h"
+#include <fstream>
+#include <cstdlib> 
+#include <iostream>
+#include <string>
 
-std::ostream& formatter(std::ostream& out, const std::string& message)
-{
-    return out << formatter(message);
+void formatter(std::ostream& out, const std::string& text) {
+    out << text << std::endl;
+    
+    const char* log_path = std::getenv("LOG_PATH");
+    if (log_path) {
+        std::ofstream log_file(log_path, std::ios::app);
+        if (log_file.is_open()) {
+            log_file << text << std::endl;
+        }
+    }
 }
